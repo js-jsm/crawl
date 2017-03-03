3f14eeeb0510f4213b8946bb9887d25138d1fe17
Ji eunjung <jieunjung@Jiui-MacBook-Pro.local>
2017-03-03 23:34:41 +0900

    eunjung update

--- eunjung/ch02/03-getall/p68-mkdir3.js
+++ eunjung/ch02/03-getall/p68-mkdir3.js
@@ -1,3 +1,8 @@
+// mkdir() 및 mkdirSync() 함수의 경우
+// 이미 존재하는 폴더를 생성하려고 하면 에러가 발생한다.
+// existsSync() 함수는 해당 폴더가 이미 존재하는지의 여부를
+// true, false 로 반환해주어 에러처리를 손쉽게 할 수 있다.
+
 var fs = require('fs');
 
 if(!fs.existsSync('test3')) {
