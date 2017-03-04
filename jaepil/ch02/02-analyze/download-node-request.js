28a57156ef7511832288990491a345458f9c8420
gomugom <power4ce@gmail.com>
2017-03-04 23:54:24 +0900

    파일 정리

--- jaepil/ch02/02-analyze/download-node-request.js
+++
@@ -1,7 +0,0 @@
-let request = require('request'),
-    fs = require('fs'),
-    url = 'http://jpub.tistory.com/',
-    savepath = 'download-node-request.html';
-
-request(url).pipe(fs.createWriteStream(savepath));
-
--- jaepil/ch02/04-xmlrss/test-xml0.js
+++
@@ -1,6 +0,0 @@
-let parseString = require('xml2js').parseString,
-    xml = "<item>Apple</item>";
-
-parseString(xml, (err, result) => {
-    console.log(JSON.stringify(result.item));
-});
